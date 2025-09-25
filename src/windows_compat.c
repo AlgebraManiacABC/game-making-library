30a2402d850888c074147a2646cf7ee63f3438d2
AlgebraManiacABC <dunnluke01@gmail.com>
2025-09-24 20:41:49 -0500

    vasprintf for MSVC since it is not mingw

--- src/windows_compat.c
+++ src/windows_compat.c
@@ -10,9 +10,28 @@ int asprintf(char ** str, const char * fmt, ...)
 	return retval;
 }
 
-int vasprintf(char ** str, const char * fmt, va_list args)
-{
-	return __mingw_vasprintf(str,fmt,args);
-}
+/* ---- MSVC vasprintf fallback ---- */
+#if defined(_MSC_VER) && !defined(__clang__)
+	#include <stdarg.h>
+	#include <stdio.h>
+	#include <stdlib.h>
+	int vasprintf(char **strp, const char *fmt, va_list ap) {
+		va_list ap2; va_copy(ap2, ap);
+		int needed = _vscprintf(fmt, ap2);
+		va_end(ap2);
+		if (needed < 0) { *strp = NULL; return -1; }
+		char *buf = (char*)malloc((size_t)needed + 1);
+		if (!buf) { *strp = NULL; return -1; }
+		int written = vsnprintf(buf, (size_t)needed + 1, fmt, ap);
+		if (written < 0) { free(buf); *strp = NULL; return -1; }
+		*strp = buf;
+		return written;
+	}
+#else
+	int vasprintf(char ** str, const char * fmt, va_list args)
+	{
+		return __mingw_vasprintf(str,fmt,args);
+	}
+#endif
 
 #endif

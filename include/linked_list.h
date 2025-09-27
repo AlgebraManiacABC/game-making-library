d7d5e538b0ced0c5605fe934a25ee6bcc103b658
AlgebraManiacABC <dunnluke01@gmail.com>
2025-09-27 12:31:04 -0500

    Add Linked Lists - WIP

---
+++ include/linked_list.h
@@ -0,0 +1,109 @@
+/********************************************************************************
+ * linked_list.h
+ * 
+ ********************************************************************************/
+
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+#include <stddef.h>
+
+typedef struct gm_Node_s gm_Node_t;
+typedef struct gm_List_s gm_List_t;
+
+/**
+ * Creates a new (empty) Linked List
+ * @returns the new `gm_List_t *`
+ */
+gm_List_t * gm_createList();
+
+/**
+ * Gets the list's node count
+ * @param list the list to query
+ * @returns the number of nodes in the provided list
+ */
+size_t gm_getListLength(gm_List_t * list);
+
+/**
+ * Destroys all nodes of and
+ * frees all memory associated with a list
+ * @param list the list to destroy
+ */
+void gm_destroyList(gm_List_t * list);
+
+/**
+ * Inserts a node at the head of the list,
+ *  filling it with the specified data
+ * @param list the list to change
+ * @param data the data to insert
+ * @returns EXIT_SUCCESS or EXIT_FAILURE
+ */
+int gm_insertNodeAtHead(gm_List_t * list, void * data);
+
+/**
+ * Gets the data from the node at the given index
+ * @param list the list from which to get data
+ * @param index the index of the data node
+ * @returns the data at the given index
+ */
+void * gm_getNodeAtIndex(gm_List_t * list, size_t index);
+
+/**
+ * Gets the data from the head node of the list
+ * @param list the list from which to get data
+ * @return the data at the head node
+ */
+void * gm_getNodeAtHead(gm_List_t * list);
+
+/**
+ * Gets the data from the tail (final) node of the list
+ * @param list the list from which to get data
+ * @return the data at the tail node
+ */
+void * gm_getNodeAtTail(gm_List_t * list);
+
+/**
+ * Remove a node by index, returning its data
+ * @param list the list to change
+ * @param index the index of the node to delete
+ * @return the data from the deleted node
+ */
+void * gm_removeNodeAtIndex(gm_List_t * list, size_t index);
+
+/**
+ * Remove the head node of a list, returning its data
+ * @param list the list to change
+ * @return the data from the deleted node
+ */
+void * gm_removeNodeAtHead(gm_List_t * list);
+
+/**
+ * Remove the tail (final) node of a list, returning its data
+ * @param list the list to change
+ * @return the data from the deleted node
+ */
+void * gm_removeNodeAtTail(gm_List_t * list);
+
+/**
+ * Searches through the list for a node with matching data,
+ *  then removes it.
+ * @param list the list to change
+ * @param data a pointer to compare with each node
+ * @returns EXIT_SUCCESS on node removal, else EXIT_FAILURE (see gm_getError)
+ */
+int gm_removeNodeMatchingData(gm_List_t * list, void * data);
+
+/**
+ * Create a new node with data at the end of a list
+ * @param list the list to change
+ * @param data the data to append
+ */
+void gm_pushObject(gm_List_t * list, void * data);
+
+/**
+ * Destroy the last node at the end of a list, returning its data
+ * @param list the list to change
+ * @returns the data extracted from the node
+ */
+void * gm_popObject(gm_List_t * list);
+
+#endif
---
+++ src/linked_list.c
@@ -0,0 +1,119 @@
+/********************************************************************************
+ * linked_list.c
+ * Goal is to create a circularly-linked list:
+ *   o<->o<->o
+ *  /         \
+ * o           o <- head
+ *  \         /
+ *   o<->o<->o
+ ********************************************************************************/
+
+#include <stdlib.h>
+#include "linked_list.h"
+
+#include "debug.h"
+
+struct gm_Node_s
+{
+    gm_Node_t * prev;
+    gm_Node_t * next;
+    void * data;
+};
+
+struct gm_List_s
+{
+    gm_Node_t * head;
+    size_t length;
+};
+
+gm_List_t * gm_createList()
+{
+    // calloc sets everything to NULL or 0
+    gm_List_t * list = calloc(1, sizeof(gm_List_t));
+    if (!list)
+    {
+        gm_setError(ERR_CODE,ERR_NOMEM);
+    }
+    return list;
+}
+
+gm_Node_t * gm_createNode(void * data)
+{
+    // calloc sets next/pref to NULL
+    gm_Node_t * node = calloc(1, sizeof(gm_Node_t));
+    if (!node)
+    {
+        gm_setError(ERR_CODE,ERR_NOMEM);
+        return NULL;
+    }
+    node->data = data;
+    return node;
+}
+
+size_t gm_getListLength(gm_List_t * list)
+{
+    if (!list) return 0;
+    return list->length;
+}
+
+void gm_destroyList(gm_List_t * list)
+{
+    if (!list) return;
+    while (list->head) gm_removeNodeAtHead(list);
+    free(list);
+}
+
+int gm_insertNodeAtHead(gm_List_t * list, void * data)
+{
+    if (!list)
+    {
+        gm_setError(ERR_CODE,ERR_NULLP);
+        return EXIT_FAILURE;
+    }
+    gm_Node_t * node = gm_createNode(data);
+    if (!node) return EXIT_FAILURE;
+    if (!list->head)
+    {
+        list->head = node;
+        node->next = node;
+        node->prev = node;
+    }
+    else
+    {
+        list->head->prev->next = node;
+        list->head->prev = node;
+        list->head = node;
+    }
+    list->length++;
+    return EXIT_SUCCESS;
+}
+
+// void * gm_getNodeAtIndex(gm_List_t * list, size_t index);
+
+// void * gm_getNodeAtHead(gm_List_t * list);
+
+// void * gm_getNodeAtTail(gm_List_t * list);
+
+// void * gm_removeNodeAtIndex(gm_List_t * list, size_t index);
+
+void * gm_removeNodeAtHead(gm_List_t * list)
+{
+    if (!list || !list->length) return NULL;
+    void * data = list->head->data;
+    list->head->prev->next = list->head->next;
+    list->head->next->prev = list->head->prev;
+    gm_Node_t * temp = NULL;
+    if (list->length > 1) temp = list->head->next;
+    free(list->head);
+    list->head = temp;
+    list->length--;
+    return data;
+}
+
+// void * gm_removeNodeAtTail(gm_List_t * list);
+
+// int gm_removeNodeMatchingData(gm_List_t * list, void * data);
+
+// void gm_pushObject(gm_List_t * list, void * data);
+
+// void * gm_popObject(gm_List_t * list);
